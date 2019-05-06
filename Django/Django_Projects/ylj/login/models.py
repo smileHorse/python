ad1f52b37c4722243b23ec1c2ac721831f021106
smileHorse <65022492@qq.com>
2019-05-06 22:06:42 +0800

    django

--- Django/Django_Projects/ylj/login/models.py
+++ Django/Django_Projects/ylj/login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	id = models.IntegerField(primary_key=True)
+	password = models.CharField(max_length=150)
+	name = models.CharField(max_length=64)
+	usertype = models.IntegerField()
+	username = models.CharField(max_length=64)
+	factory = models.CharField(max_length=60)
